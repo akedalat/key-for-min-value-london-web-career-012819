66fad926c8c729e8253c3ff8d18ac660f0998e66
Kian Edalat <akedalat@gmail.com>
2019-01-17 15:07:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,15 @@
 
 def key_for_min_value(name_hash)
 
+name_hash.find do |name, value|
+  min = value
+
+  name_hash.collect do |name,value|
+    if name < min
+      min = value
+    end
+  end
+    return name
+
+end
 end
